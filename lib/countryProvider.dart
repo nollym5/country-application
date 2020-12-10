fb4ad4ed80bd28679af94b173d876535f12cf715
nolwazi <nollym5@gmail.com>
2020-12-11 01:16:04 +0200

    search bar

--- lib/countryProvider.dart
+++ lib/countryProvider.dart
@@ -3,35 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class CountryData {
-  // String code;
-  // int population;
-  // String subRegion;
-  // double area;
-  // String demonym; //people called
-  // double gini;
-  // String currency;
-  // String symbol;
-  // String capitalCity;
-  // String languages;
-  String country;
-
-  CountryData(
-      {
-        //this.capitalCity,
-        // this.subRegion,
-        this.country,
-        // this.symbol,
-        // this.currency,
-        // this.demonym,
-        // this.population,
-        // this.area,
-        // this.code,
-        // this.gini,
-        // this.languages
-      });
-}
-
 class CountryProvider with ChangeNotifier {
   List<dynamic> _list;
   var countryDetails;
@@ -43,14 +14,14 @@ class CountryProvider with ChangeNotifier {
     if (response.statusCode == 200) {
       _list = jsonDecode(response.body);
       _country=_list;
-      print(_list);
+
 
     } else {
       _list=null;
-      print(_list);
+
     }
     return _list;
   }
 
-  List<dynamic> get country => _country;
+
 }
